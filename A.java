bf4ff5194ea2be98489975d93f213df5e76f4eca
sebasrevuelta <122784773+sebasrevuelta@users.noreply.github.com>
2025-01-31 09:55:28 +0100

    Update A.java

--- A.java
+++ A.java
@@ -1,10 +1,12 @@
 package org.testcode;
 
 import javax.servlet.http.HttpServletResponse;
+import org.util.C;
 
 public class A {
 
     public void suspiciousCall(java.sql.Statement statement, String sql, HttpServletResponse response) throws java.sql.SQLException {
+        sql = C.reviewQuery(sql);        
         int count = statement.executeUpdate(sql);
         org.owasp.benchmark.helpers.DatabaseHelper.outputUpdateComplete(sql, response);
     }
